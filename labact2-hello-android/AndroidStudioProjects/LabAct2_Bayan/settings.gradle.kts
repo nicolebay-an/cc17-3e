6df0e1091d13b9d62af0b8d27ed77dbeafb12990
Nikoru Beian <kiahden22@gmail.com>
2023-09-27 21:25:13 +0800

    Second commit

---
+++ labact2-hello-android/AndroidStudioProjects/LabAct2_Bayan/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LabAct2_Bay-an"
+include(":app")
+ 
